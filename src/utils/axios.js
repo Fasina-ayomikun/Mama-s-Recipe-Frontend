eb6c3efe1f1011b9d2c822480effa14895c39049
Deeyah <101244317+Fasina-ayomikun@users.noreply.github.com>
2023-02-25 11:13:43 +0100

    Update axios.js

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,7 +3,7 @@ const customUrl = axios.create({
   baseURL: process.env.REACT_APP_SERVER_URL,
    headers:{
       'Access-Control-Allow-Origin': 'https://nutty-bass-nightshirt.cyclic.app',
-      }
+     }
  
 });
 
