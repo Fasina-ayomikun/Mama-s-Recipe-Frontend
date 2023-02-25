2d84f830813442ef326573b7625af366f1351156
Deeyah <101244317+Fasina-ayomikun@users.noreply.github.com>
2023-02-25 12:38:00 +0100

    Update axios.js

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 const customUrl = axios.create({
   baseURL: process.env.REACT_APP_SERVER_URL,
-  
+   headers:{
+      'Access-Control-Allow-Origin':process.env.REACT_APP_SERVER_URL ,
+     }
+ 
 });
 
 export { customUrl };
