3363f45e658a03dd3e895df9d84439b9f90d5540
Deeyah <101244317+Fasina-ayomikun@users.noreply.github.com>
2023-02-27 12:32:26 +0100

    Update filesThunk.js

--- src/features/files/filesThunk.js
+++ src/features/files/filesThunk.js
@@ -5,10 +5,10 @@ import { checkUserAuthorization } from "../../utils/functions";
 const imageThunk = async (url, file, thunkAPI) => {
   try {
     const resp = await axios.post(`${process.env.REACT_APP_SERVER_URL}${url}`, file, {
-      
       headers: {
+         'Access-Control-Allow-Origin':process.env.REACT_APP_SERVER_URL ,
         "Content-Type": "multipart/form-data",
-        'Access-Control-Allow-Origin':'*' ,
+   
      
       },
       withCredentials: true,
