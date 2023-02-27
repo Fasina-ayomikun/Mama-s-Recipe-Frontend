85ba8c6f944a0d95014423b589a785bbe947ff1f
Deeyah <101244317+Fasina-ayomikun@users.noreply.github.com>
2023-02-27 08:38:38 +0100

    Update ProtectedRoute.js

--- src/utils/ProtectedRoute.js
+++ src/utils/ProtectedRoute.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navigate, useNavigate } from "react-router-dom";
+import { Navigate} from "react-router-dom";
 import { toast } from "react-toastify";
 
 function ProtectedRoute({ children, id }) {
