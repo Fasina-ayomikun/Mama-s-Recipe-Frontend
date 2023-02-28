0ada59b8803b0807773021e330c5eab2e1837152
Deeyah <101244317+Fasina-ayomikun@users.noreply.github.com>
2023-02-28 09:34:39 +0100

    Update filesThunk.js

--- src/features/files/filesThunk.js
+++ src/features/files/filesThunk.js
@@ -8,7 +8,7 @@ const imageThunk = async (url, file, thunkAPI) => {
  const resp = await axios.post('https://nutty-bass-nightshirt.cyclic.app/files/upload/image', file, {
   headers: {
     "Content-Type": "multipart/form-data",
-    "Access-Control-Allow-Origin": 'https://mama-s-recipe.vercel.app',},
+    "Access-Control-Allow-Origin": '*',},
   withCredentials: true,
 });
     console.log(resp.data)
