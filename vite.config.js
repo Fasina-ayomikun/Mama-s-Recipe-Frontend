124b56e8222849211a5df3d5e743401972745977
Fasina Ayomikun <ayomikunfasina240@gmail.com>
2024-03-16 21:46:15 +0100

    fixed tailwind css not working

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react";
 import svgrPlugin from "vite-plugin-svgr";
-
+import tailwindcss from "tailwindcss";
 // https://vitejs.dev/config/
 export default defineConfig({
   // This changes the out put dir from dist to build
@@ -17,6 +17,7 @@ export default defineConfig({
         // ...svgr options (https://react-svgr.com/docs/options/)
       },
     }),
+    tailwindcss(),
   ],
   test: {
     globals: true,
